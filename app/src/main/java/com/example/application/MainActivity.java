e9e0f8e758e7f8c11f7af68c20d29d90c94f6b39
Rashindra yadav <yadavrashindra5@gmail.com>
2022-06-23 09:38:33 +0530

    change in oncreate method

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
     }
 }
